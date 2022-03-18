f51d0439ba200df7b2146bafd11461618e68fe9b
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 11:32:17 +0500

    Inject services in program class

--- WebEmploye.API/Program.cs
+++ WebEmploye.API/Program.cs
@@ -10,7 +10,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddTransient<IEmployeRepo, EmployeRepo>();
+builder.Services.AddTransient<IDepartmentRepo, DepartmentRepo>();
 builder.Services.AddControllers();
+builder.Services.AddMvcCore().AddApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
