c6ba4653620c7d7f7f4c4248d315879b1f6530aa
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 08:33:18 +0500

    Hence create employee interface and declare its all method

---
+++ WebEmploye.API/Infrastructure/IEmployeRepo.cs
@@ -0,0 +1,14 @@
+﻿using WebEmp_DLL.Entities;
+
+namespace WebEmploye.API.Infrastructure
+{
+    public interface IEmployeRepo
+    {
+        Task<IEnumerable<Employee>> Search(string name);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetById(int id);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(int id);
+    }
+}
