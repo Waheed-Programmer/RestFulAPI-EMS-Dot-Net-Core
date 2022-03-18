c5f7372c939a42dd087dd3ac7f70273c78d57d90
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 07:52:34 +0500

    Add project files.

---
+++ WebEmp_DLL/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebEmp_DLL.Entities
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+}
---
+++ WebEmp_DLL/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebEmp_DLL.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DateBirth  { get; set; }
+    }
+
+    public enum Gender 
+    { 
+    Male, Female, Other    
+    }
+
+}
