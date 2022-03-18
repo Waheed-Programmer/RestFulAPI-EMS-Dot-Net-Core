9ddaeb75b4f8c407f124772b983fa6fa0991b0dd
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 09:53:44 +0500

    create department services and interfaces

---
+++ WebEmploye.API/Infrastructure/IDepartmentRepo .cs
@@ -0,0 +1,14 @@
+﻿using WebEmp_DLL.Entities;
+
+namespace WebEmploye.API.Infrastructure
+{
+    public interface IDepartmentRepo
+    {
+        Task<IEnumerable<Department>> Search(string name);
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetById(int id);
+        Task<Department> AddDepartment(Department department);
+        Task<Department> UpdateDepartment(Department department);
+        Task<Department> DeleteDepartment(int id);
+    }
+}
---
+++ WebEmploye.API/Services/DepartmentRepo.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebEmp_DLL.Data;
+using WebEmp_DLL.Entities;
+using WebEmploye.API.Infrastructure;
+
+namespace WebEmploye.API.Services
+{
+    public class DepartmentRepo : IDepartmentRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DepartmentRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Department> AddDepartment(Department Department)
+        {
+            var result = await _context.departments.AddAsync(Department);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Department> DeleteDepartment(int id)
+        {
+
+            var result = await _context.departments.FirstOrDefaultAsync(x => x.DepartmentId == id);
+            if (result != null)
+            {
+               _context.departments.Remove(result);
+                await _context.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+
+        public async Task<Department> GetById(int id)
+        {
+            return await _context.departments.Include(a=>a.EmployeeDepartment).ThenInclude(m=>m.Department)
+                .FirstOrDefaultAsync(x=>x.DepartmentId == id);
+        }
+
+        public async Task<IEnumerable<Department>> GetDepartments()
+        {
+            return await _context.departments.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Department>> Search(string name)
+        {
+            IQueryable<Department> query = _context.departments;
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(x => x.DepartmentName.Contains(name));
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<Department> UpdateDepartment(Department department)
+        {
+            var result = await _context.departments.FirstOrDefaultAsync(x=>x.DepartmentId==department.DepartmentId);
+            if (result != null)
+            {
+                //result.DepartmentName = department.DepartmentName;
+                //result.DateBirth = department.DateBirth;
+                //result.Gender = department.Gender;
+                await _context.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        } 
+    }
+}
