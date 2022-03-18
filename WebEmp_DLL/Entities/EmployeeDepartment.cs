f29d65f2bc81d21ef58d4e22df5d72e3cf0913d5
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 07:53:55 +0500

    Initial Commit

--- WebEmp_DLL/Entities/EmployeeDepartment.cs
+++ WebEmp_DLL/Entities/EmployeeDepartment.cs
@@ -8,5 +8,10 @@ namespace WebEmp_DLL.Entities
 {
     public class EmployeeDepartment
     {
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
     }
 }
