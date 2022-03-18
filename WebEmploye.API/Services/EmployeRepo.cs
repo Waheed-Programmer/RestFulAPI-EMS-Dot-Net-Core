ecdaec721eec7beaec0a1782b2d475db19e8df90
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-18 08:36:36 +0500

    add new class and implement of all interface mathods

---
+++ WebEmploye.API/Services/EmployeRepo.cs
@@ -0,0 +1,38 @@
+﻿using WebEmp_DLL.Entities;
+using WebEmploye.API.Infrastructure;
+
+namespace WebEmploye.API.Services
+{
+    public class EmployeRepo : IEmployeRepo
+    {
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> Search(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
